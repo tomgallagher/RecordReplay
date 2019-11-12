47df7067b7882f9bad49836af04004efd69bbe80
Tom Gallagher <thomasgallagher101@gmail.com>
2019-11-12 16:38:59 +0000

    added new project form

---
+++ user_interface/newProject.js
@@ -0,0 +1,32 @@
+$(document).ready (function(){
+
+    $('#newProjectForm').form({
+        on: 'blur',
+        fields: {
+            projectName: {
+                identifier: 'projectName',
+                rules: [
+                    { type : 'empty', prompt : 'Please enter project name' }
+                ]
+            },
+            projectDescription: {
+                identifier: 'projectDescription',
+                rules: [
+                    { type : 'empty', prompt : 'Please enter project description' }
+                ]
+            },
+            projectAuthor: {
+                identifier: 'projectAuthor',
+                rules: [
+                    { type : 'empty', prompt : 'Please identify project owner' }
+                ]
+            }
+        },
+        onSuccess(event, fields) {
+            event.preventDefault();
+            console.log(fields);
+            $('#newProjectForm').form('clear')
+        }
+    });
+
+});
