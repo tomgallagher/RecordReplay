771433a6aa48793b0621e9d278d3178d0dc991f5
Tom Gallagher <thomasgallagher101@gmail.com>
2019-11-23 17:56:50 +0000

    small comments addition

--- models/activeRecordingModel.js
+++ models/activeRecordingModel.js
@@ -33,12 +33,15 @@ class ActiveRecording extends Recording {
             .concatMap(chromeUrl => Rx.Observable.ajax({url: chromeUrl, responseType: "text"}) )
             //then we map the responses into the strings
             .map(data => data.response.toString())
-            //then we add all the text together - use reduce rather than scan as we only need the finial emission from a finite observable
+            //then we add all the text together - use reduce rather than scan as we only need the final emission from a finite observable
             .reduce((previousScriptText, currentScriptText) => previousScriptText + "\n\n" + currentScriptText)
             //then when we have finished, we will get an emission with reduce
             .subscribe(
+                //save the collected script string to the model property so we can inject all scripts as a single string into Remote Devtools Protocol controlled tab instance
                 collectedText => this.recordingScriptsString = collectedText,
+                //log any errors obviously
                 error => console.error(error),
+                //then we log the completion of this event to ensure that the async task is not completing after its output might be required
                 () => console.log("Active Recording has Collected ALL scripts into recordingScriptsString")
             );
 
